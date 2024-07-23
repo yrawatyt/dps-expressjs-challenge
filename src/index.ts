3ee9b2067c636a8fd18f844ff3b3af5f908e4212
yrawatyt <110770941+yrawatyt@users.noreply.github.com>
2024-07-23 22:45:01 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express, { Express } from 'express';
-import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import projectRoutes from './routes/project.routes';
+import reportRoutes from './routes/report.routes';
 
-dotenv.config();
+const app = express();g
+const PORT = 3000;
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 
-app.use(express.json());
+app.use('/projects', projectRoutes);
+app.use('/reports', reportRoutes);
 
-app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
+
